1d4ff916558bc7035ad73dbe5f4e293d1e288fe8
Kaiser Li <kaiserli@gmail.com>
2019-10-09 12:46:56 +0800

    add run grpc server with graceful stop

--- go.mod
+++ go.mod
@@ -10,4 +10,5 @@ require (
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.4.0
+	google.golang.org/grpc v1.21.0
 )
--- qhttp/server.go
+++ qhttp/server.go
@@ -2,15 +2,62 @@ package qhttp
 
 import (
 	"context"
+	"net"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 )
 
 var (
 	log = logrus.WithField("pkg", "qhttp")
 )
+
+// this function not work without tls enabled
+// refer to https://github.com/dhrp/grpc-rest-go-example
+func GrpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO(tamird): point to merged gRPC code rather than a PR.
+		// This is a partial recreation of gRPC's internal checks https://github.com/grpc/grpc-go/pull/514/files#diff-95e9a25b738459a2d3030e1e6fa2a718R61
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	})
+}
+
+func RunGRPCServer(ctx context.Context, addr string, s *grpc.Server) (err error) {
+	srvErrChan := make(chan error)
+
+	go func() {
+		lis, err := net.Listen("tcp", addr)
+		if err != nil {
+			log.WithError(err).Error("GRPC server listen fail")
+		}
+
+		srvErrChan <- s.Serve(lis)
+	}()
+
+	select {
+	case err = <-srvErrChan:
+		return err
+	case <-ctx.Done():
+		shutDownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		go func() {
+			s.GracefulStop()
+			log.Debug("GRPC server gracefully exit")
+			cancel()
+		}()
+
+		<-shutDownCtx.Done()
+		return nil
+	}
+}
 
 // RunServer run a http server with gracefully shutdown
 func RunServer(ctx context.Context, addr string, handler http.Handler, opts ...func(*http.Server)) (err error) {
