e10edcafcc40d76a08994f2c801a31c1aa138c4f
Kaiser Li <kaiserli@gmail.com>
2020-03-30 19:55:43 +0800

    add qhttp client request option func

--- qhttp/client.go
+++ qhttp/client.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
@@ -34,34 +35,69 @@ var (
 	}
 )
 
+// WithAuthorization add authorization bearer to request
+func WithAuthorization(token string) func(*http.Request) {
+	return func(req *http.Request) {
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+}
+
 // Get method
-func Get(url string) (resp *http.Response, err error) {
-	return QHTTPClient.Get(url)
+func Get(url string, reqOpts ...func(*http.Request)) (resp *http.Response, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range reqOpts {
+		f(req)
+	}
+
+	return QHTTPClient.Do(req)
 }
 
 // Post method
-func Post(url, contentType string, body io.Reader) (resp *http.Response, err error) {
-	return QHTTPClient.Post(url, contentType, body)
+func Post(url, contentType string, body io.Reader, reqOpts ...func(*http.Request)) (resp *http.Response, err error) {
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", contentType)
+
+	for _, f := range reqOpts {
+		f(req)
+	}
+
+	return QHTTPClient.Do(req)
 }
 
 // PostForm method
-func PostForm(url string, data url.Values) (resp *http.Response, err error) {
-	return QHTTPClient.PostForm(url, data)
+func PostForm(url string, data url.Values, reqOpts ...func(*http.Request)) (resp *http.Response, err error) {
+	return Post(url, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()), reqOpts...)
 }
 
 // Head method
-func Head(url string) (resp *http.Response, err error) {
-	return QHTTPClient.Head(url)
+func Head(url string, reqOpts ...func(*http.Request)) (resp *http.Response, err error) {
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	for _, f := range reqOpts {
+		f(req)
+	}
+	return QHTTPClient.Do(req)
 }
 
 // PostJSON method
-func PostJSON(url string, body interface{}, result interface{}) (resp *http.Response, err error) {
+func PostJSON(url string, body interface{}, result interface{}, reqOpts ...func(*http.Request)) (resp *http.Response, err error) {
 	b, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
 	}
 
-	rsp, err := QHTTPClient.Post(url, "application/json", bytes.NewReader(b))
+	rsp, err := Post(url, "application/json", bytes.NewReader(b), reqOpts...)
+
 	if err != nil {
 		return nil, err
 	}
